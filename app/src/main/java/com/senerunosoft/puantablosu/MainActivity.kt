e6c426aa3c4b8db60b3dee1355cc3af35d3fbb35
onur9848 <onur9848@hotmail.com>
2025-08-07 00:14:22 +0300

    fix

--- app/src/main/java/com/senerunosoft/puantablosu/MainActivity.kt
+++ app/src/main/java/com/senerunosoft/puantablosu/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
