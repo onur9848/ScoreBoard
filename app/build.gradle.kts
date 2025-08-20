9df9b355ecca6778c5dc68fd246ef16f1be71117
onur9848 <onur9848@hotmail.com>
2025-08-20 19:37:28 +0300

    versionUpdate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.senerunosoft.puantablosu"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
